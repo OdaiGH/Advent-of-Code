65afae9d886b4bb6951d5f95cd3116a892d1ca16
Odai Alghamdi <pr.boy@hotmail.it>
2022-12-17 00:42:42 +0300

    Update Rucksack.py

--- Day 3/Rucksack.py
+++ Day 3/Rucksack.py
@@ -1,4 +1,4 @@
-def finalScore(input) -> int:
+def finalScore_1(input) -> int:
     sum = 0
     with open(input) as file:
         for line in file:
@@ -11,7 +11,27 @@ def finalScore(input) -> int:
                 else:
                     sum += ord(letter) - 96
         return sum
+
+
+def finalScore_2(input) -> int:
+    sum = 0
+    with open(input) as file:
+        lines = file.readlines()
+
+    lines = [line.strip() for line in lines]
+    for line in range(0,len(lines),3):
+        s1 = set(lines[line])
+        s2 = set(lines[line+1])
+        s3 = set(lines[line+2])
+        commonLetters = list(s1&s2&s3)
+        for letter in commonLetters:
+                if letter.isupper():
+                    sum += ord(letter) - 38
+                else:
+                    sum += ord(letter) - 96
+    return sum
     
 
 
-print(finalScore("input.txt"))
+print(finalScore_1("input.txt"))
+print(finalScore_2("input.txt"))
