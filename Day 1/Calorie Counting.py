8c6c92e9bf01a8ffb4efebff369bc6767ea60cbd
Odai Alghamdi <pr.boy@hotmail.it>
2022-12-18 01:10:42 +0300

    added part 2 for question 1
    added part 1

--- Day 1/Calorie Counting.py
+++ Day 1/Calorie Counting.py
@@ -1,6 +1,6 @@
 def maxCalroieCounting(input) -> int:
 	values = []
-	with open("input.txt") as file:
+	with open(input) as file:
 		for line in file:
 			if line == "\n":
 				values.append(0)
@@ -11,4 +11,20 @@ def maxCalroieCounting(input) -> int:
 			values[len(values)-1]+= int(line)
 	return max(values)
 
+def maxCalroieCounting_2(input) -> int:
+	values = []
+	sum = 0 
+	with open(input) as file:
+		for line in file:
+			if line == "\n":
+				values.append(0)
+				continue
+			if not values:
+				values.append(int(line))
+				continue
+			values[len(values)-1]+= int(line)
+	values = sorted(values)
+	return values[-1]+values[-2]+values[-3]
+
 print(maxCalroieCounting("input.txt"))
+print(maxCalroieCounting_2("input.txt"))
